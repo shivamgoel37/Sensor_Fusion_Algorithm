5ae3f8c6a088f4c0fe4ec244918831477af72ca9
Kunal Markan <kunalmarkan@Kunals-MacBook-Pro.local>
2019-12-11 18:44:28 -0500

    updated code, added initial steps in algorithm --> support deg. matrix, eigen vals and vectors, principal components

--- src/program_final.c
+++ src/program_final.c
@@ -1,5 +1,5 @@
-//#include <string.h>
 //#include <stdio.h>
+//#include <string.h>
 //#include <math.h>
 //#include <gsl/gsl_math.h>
 //#include <gsl/gsl_eigen.h>
@@ -55,4 +55,75 @@ double myAbs(double f)
 {
     return (f>0)?f:-f;
 }
+void fusedvalue(FILE* fp, char* prevtime)
+{
+    
+    //STEP 1-calculate D
+    int i, j, k; 
+    double data[SENSOR_COUNT*SENSOR_COUNT];
 
+
+    
+    fprintf(fp,"%s,",prevtime);
+    
+    
+
+    for(i=0;i<SENSOR_COUNT;i++)
+    {
+        for(j=0;j<SENSOR_COUNT;j++)
+        {    
+            mtx_d[i][j]= pow(e,-1*abs(sensor_values[i]-sensor_values[j]));
+            
+            data[i*SENSOR_COUNT+j]=pow(e,-1*abs(sensor_values[i]-sensor_values[j]));       
+        }
+        
+    }
+
+    //STEP 2- gsl eigenvector calculate
+    gsl_matrix_view m = gsl_matrix_view_array (data, SENSOR_COUNT, SENSOR_COUNT);
+    gsl_vector *eval = gsl_vector_alloc (SENSOR_COUNT);
+    gsl_matrix *evec = gsl_matrix_alloc (SENSOR_COUNT, SENSOR_COUNT);
+    gsl_eigen_symmv_workspace * ew = gsl_eigen_symmv_alloc (SENSOR_COUNT);
+       
+    gsl_eigen_symmv(&m.matrix, eval, evec, ew);
+     
+    gsl_eigen_symmv_free (ew);
+     
+    gsl_eigen_symmv_sort (eval, evec, GSL_EIGEN_SORT_ABS_ASC);
+
+    
+    double evals[SENSOR_COUNT];
+    double evectors[SENSOR_COUNT][SENSOR_COUNT];
+
+    
+    for (i = 0; i < SENSOR_COUNT; i++)
+    {
+        evals[i] = gsl_vector_get (eval, i);
+        gsl_vector_view evetc= gsl_matrix_column (evec, i);
+        gsl_vector v = evetc.vector;
+
+
+        for(j=0;j<SENSOR_COUNT;j++)
+        {
+            evectors[i][j]=gsl_vector_get(&v,j);
+        }     
+       
+    }
+    
+    //STEP 3
+    double y[SENSOR_COUNT];
+
+    for(i=0;i<SENSOR_COUNT;i++)
+    {
+        y[i]=0;
+        for(j=0;j<SENSOR_COUNT;j++)
+        {
+            for(k=0;k<SENSOR_COUNT;k++)
+            {
+                y[i]+=evectors[i][j]*mtx_d[k][j];
+            }
+        }
+    }
+    
+    
+    
