b71630f89c474fdac0ea029720f94b3920baeb70
unknown <singhprab31@gmail.com>
2019-12-11 18:28:48 -0500

    Added external header file and added some code

--- include/main.h
+++ include/main.h
@@ -1,1 +1,32 @@
+#ifndef __MAIN__H__
+#define __MAIN__H__
 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_eigen.h>
+
+#define SENSOR_COUNT 3
+
+/**
+ * \brief Function to set the values of each element of the degree matrix to "0"
+ * Takes SENSOR_COUNT as the test condition parameter
+ * Clears any previous sensor values
+ */
+void init() {
+    int i,j;
+    for(i=0;i<SENSOR_COUNT;i++) {
+        
+        for(j=0;j<SENSOR_COUNT;j++) {
+            
+            mtx_d[i][j]=0;
+        }
+    }
+    
+    for(i=0;i<SENSOR_COUNT;i++) {
+        sensor_values[i]=0;
+    }
+}
+
+#endif
--- src/program_final.c
+++ src/program_final.c
@@ -1,1 +1,58 @@
+//#include <string.h>
+//#include <stdio.h>
+//#include <math.h>
+//#include <gsl/gsl_math.h>
+//#include <gsl/gsl_eigen.h>
+//#define SENSOR_COUNT 3
+
+#include "../include/main.h"
+
+double mtx_d[SENSOR_COUNT][SENSOR_COUNT];
+
+double sensor_values[SENSOR_COUNT];
+double prev_sensor_values[SENSOR_COUNT];
+char tokens[3][1024];
+double e = 2.718281828459;
+
+void clearTokens(char tokens[][1024])
+{
+    int i;
+    for(i=0;i<3;i++)
+    {        
+        tokens[i][0]='\0';
+    }
+}
+
+void copy_to_prev()
+{
+    int i;
+    for(i=0;i<SENSOR_COUNT;i++)
+    {        
+        
+        prev_sensor_values[i]=sensor_values[i];        
+    }
+}
+void takeTokensValue()
+{
+    //sens1,10 -> sensor_value[0]=10
+    char sensorname[100];
+    int i;
+    int j=0;
+    int sensorno;
+    double sensorvalue;
+
+    for(i=4;i<strlen(tokens[1]);i++)
+        sensorname[j++]=tokens[1][i];
+    sensorname[j]='\0';
+
+    sensorno=atoi(sensorname);
+    sensorvalue = atof(tokens[2]);
+    sensor_values[sensorno-1]=sensorvalue;    
+
+    
+}
+double myAbs(double f)
+{
+    return (f>0)?f:-f;
+}
 
